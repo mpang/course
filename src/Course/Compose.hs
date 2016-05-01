d1b2515dbb7dbedcbf0e5b3fbc0f5908ed0aeca7
Meng Pang <pangmeng0218@gmail.com>
2016-05-01 10:47:07 -0600

    more Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -8,6 +8,10 @@ import Course.Functor
 import Course.Applicative
 import Course.Monad
 
+import Course.Traversable
+import Data.Foldable
+import Data.Monoid
+
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
 newtype Compose f g a =
@@ -15,17 +19,25 @@ newtype Compose f g a =
 
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) f (Compose g) = Compose ((f <$>) <$> g)
+  Functor (Compose f g) where
+    (<$>) f (Compose fga) = Compose ((f <$>) <$> fga)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
--- Implement the pure function for an Applicative instance for Compose
-  pure = Compose . pure . pure
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) (Compose f) (Compose g) = Compose (lift2 (<*>) f g)
+    -- Implement the pure function for an Applicative instance for Compose
+    pure = Compose . pure . pure
+    -- Implement the (<*>) function for an Applicative instance for Compose
+    (<*>) (Compose fga) (Compose fga') = Compose (lift2 (<*>) fga fga')
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
--- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) = error "impossible: Course.Compose (<<=)#instance (Compose f g)"
+    -- Implement the (=<<) function for a Monad instance for Compose
+    (=<<) = error "impossible: Course.Compose (<<=)#instance (Compose f g)"
+
+instance (Foldable f, Foldable g) =>
+  Foldable (Compose f g) where
+    foldMap f (Compose fga) = foldMap (foldMap f) fga
+
+instance (Traversable f, Traversable g) =>
+  Traversable (Compose f g) where
+    traverse f (Compose fga) = pure Compose <*> traverse (traverse f) fga
